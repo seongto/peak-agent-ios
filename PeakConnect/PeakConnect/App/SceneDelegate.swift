d49d8f9d230c61162df3db8e4f005110b21f21a6
kangminseoung <alstjd2356@gmail.com>
2025-05-23 13:28:03 +0900

    [Feat] 지도에서 리드찾기 버튼 탭 시 MapViewController로 화면 전환 기능 구현 및 버튼 UI 레이아웃 수정

--- PeakConnect/PeakConnect/App/SceneDelegate.swift
+++ PeakConnect/PeakConnect/App/SceneDelegate.swift
@@ -15,7 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MapViewController()
+        
+        let mainViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        window.rootViewController = navigationController
+
         window.makeKeyAndVisible()
         self.window = window
     }

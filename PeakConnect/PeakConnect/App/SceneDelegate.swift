9fec4a8112ec6800a8e72db34eb3bdc92a84d40c
kangminseoung <alstjd2356@gmail.com>
2025-05-23 14:20:40 +0900

    [Refactor] SceneDelegate 내에서 UITabBarController 구성하도록 리팩토링

--- PeakConnect/PeakConnect/App/SceneDelegate.swift
+++ PeakConnect/PeakConnect/App/SceneDelegate.swift
@@ -15,7 +15,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = TabbarViewController()
+
+        let mainVC = UINavigationController(rootViewController: MainViewController())
+        let dummyHistoryVC = UIViewController()
+        let tabBarController = UITabBarController()
+        tabBarController.setViewControllers([mainVC, dummyHistoryVC], animated: false)
+
+        if let items = tabBarController.tabBar.items, items.count >= 2 {
+            items[0].image = UIImage(systemName: "house.fill")
+            items[0].title = "홈"
+            items[1].image = UIImage(systemName: "clock.fill")
+            items[1].title = "히스토리"
+        }
+
+        window.rootViewController = tabBarController
         window.makeKeyAndVisible()
         self.window = window
     }
@@ -42,4 +55,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-

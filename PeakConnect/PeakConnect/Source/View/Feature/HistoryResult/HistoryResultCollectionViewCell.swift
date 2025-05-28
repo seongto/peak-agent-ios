6195bffc553474309d2efccafaad92f518dda6c7
name-mun <tains9153@gmail.com>
2025-05-28 17:06:54 +0900

    [Feat] 셀 컴포넌트 구현

--- PeakConnect/PeakConnect/Source/View/Feature/HistoryResult/HistoryResultCollectionViewCell.swift
+++ PeakConnect/PeakConnect/Source/View/Feature/HistoryResult/HistoryResultCollectionViewCell.swift
@@ -13,6 +13,16 @@ class HistoryResultCollectionViewCell: UICollectionViewCell {
     
     static let id = "HistoryResultCollectionViewCell"
     
+    private let companyNameLabel = UILabel().then {
+        $0.text = "더선한 주식회사"
+        $0.font = UIFont(name: "Pretendard-Bold", size: 16)
+    }
+    
+    private let adressNameLabel = UILabel().then {
+        $0.text = "서울특별시 서초구 효령료 391"
+        $0.font = UIFont(name: "Pretendard-Regular", size: 14)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -28,5 +38,20 @@ extension HistoryResultCollectionViewCell {
     private func setupUI() {
         backgroundColor = .text
         layer.cornerRadius = 24
+        
+        [
+            companyNameLabel,
+            adressNameLabel
+        ].forEach {
+            addSubview($0)
+        }
+        
+        companyNameLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().inset(20)
+        }
+        
+        adressNameLabel.snp.makeConstraints { make in
+            make.bottom.leading.equalToSuperview().inset(20)
+        }
     }
 }
